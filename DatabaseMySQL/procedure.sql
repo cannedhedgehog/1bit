e5a3c2bcfd55fb6e08e545d671c233de57daa0f4
Vicky <125538123+Vicky-0222@users.noreply.github.com>
2025-07-10 14:13:04 +1000

    актуальные процедуры

--- DatabaseMySQL/procedure.sql
+++ DatabaseMySQL/procedure.sql
@@ -21,9 +21,9 @@ CALL request_brief(2)// -- пример вызова
 -- ------------------------------------------------------------------------------------------------------
 -- Процедура для генерации чек-листа для компании в соответствии с конфигурацией и видом деятельности *
 -- ------------------------------------------------------------------------------------------------------
-CREATE PROCEDURE generating_checklist(IN companyId INT)
+CREATE PROCEDURE generating_checklist_OV(IN companyId INT)
 BEGIN
-	SELECT c.name AS company_name, ch.title, ch.price_from, ch.price_to 
+	SELECT ch.id, ch.title, ch.description, ch.priority
 	FROM checklist ch 
 	JOIN company c ON c.id = companyId 
 	JOIN company_has_configurations chc ON (c.id = chc.company_id)
@@ -35,7 +35,7 @@ BEGIN
 	LIMIT 5;
 END //
 
-CALL generating_checklist(1)// -- пример вызова
+CALL generating_checklist_OV(2)// -- пример вызова
 
 /*
 INSERT INTO checklist_for_company (company_id, checklist_id, status, final_price, comment, date, employee_id ) VALUE (1,5,'отказано',3000,NULL, current_date(), 1) //
@@ -49,7 +49,7 @@ select * from checklist_for_company//
 CREATE PROCEDURE request_company_info (companyId INT)
 BEGIN
 	SELECT co.name AS company_name, co.TIN, co.ITS, co.industry, g.name AS group_company, co.staff, co.database_num, co.web, e.name AS equipment, co.taxation_system, co.server_license, co.maintenance_pc, co.antivirus, co.comments, 
-    tob.name, conf.name, ca.address, cc.name AS contact_name, cc.position, cc.phone_number, cc.email, cc.city, cc.work_day_of_week, cc.work_time_from, cc.work_time_to 
+    tob.name, conf.name, chc.version, chc.update_by, chc.date_of_update, ca.address, cc.name AS contact_name, cc.position, cc.phone_number, cc.email, cc.city, cc.work_day_of_week, cc.work_time_from, cc.work_time_to 
     FROM company co JOIN group_of_companies g ON (co.group_of_companies_id = g.id) JOIN equipment e ON (co.equipment_id = e.id) JOIN type_of_business_has_company tobhc ON (co.id = tobhc.company_id) 
     JOIN type_of_business tob ON (tobhc.type_of_business_id = tob.id) JOIN company_has_configurations chc ON (co.id = chc.company_id) JOIN configurations conf ON (chc.configurations_id = conf.id) 
     JOIN company_address ca ON (co.id = ca.company_id) JOIN company_contact cc ON (ca.id = cc.company_address_id) 
@@ -69,7 +69,7 @@ BEGIN
 END
 //
 
-CALL request_checklist_for_company(2, 'отказано')//
+CALL request_checklist_for_company(1, 'отказано')//
 
 
 -- -- --------------------------------------------------
@@ -77,7 +77,7 @@ CALL request_checklist_for_company(2, 'отказано')//
 -- -----------------------------------------------------
 CREATE PROCEDURE request_employee (employeeId INT)
 BEGIN
-	SELECT name, role, position, login, password FROM employee WHERE employeeId = id;
+	SELECT name, department, role, login, password FROM employee WHERE employeeId = id;
 END
 //
 
@@ -130,10 +130,20 @@ CALL request_checklist_info(1)//
 -- ---------------------------------------------------------------------------------------------------------------------
 -- Процедура для обновления чеклиста, если у инфоповода прошел период (удаляет запись из таблицы checklist_for_company) *
 -- ---------------------------------------------------------------------------------------------------------------------
-CREATE PROCEDURE cleanup_expired_checklist_for_company()
+CREATE PROCEDURE cleanup_expired_checklist_for_company1()
 BEGIN
     DELETE cfc FROM checklist_for_company cfc JOIN checklist c ON (cfc.checklist_id = c.id)
-    WHERE (cfc.status = 'отказано' OR cfc.status = 'под вопросом') AND DATE_ADD(cfc.date, INTERVAL c.duration DAY) < NOW() AND c.onetime_or_periodic = 0;
+    WHERE (cfc.status = 'отказано' OR cfc.status = 'под вопросом' ) AND DATE_ADD(cfc.date, INTERVAL c.duration DAY) < NOW() AND c.onetime_or_periodic = 0;
+END //
+
+
+-- -------------------------------------------------------------------
+-- Процедура для обновления чеклиста, если менеджер обнуляет статус
+-- -------------------------------------------------------------------
+CREATE PROCEDURE cleanup_expired_checklist_for_company2()
+BEGIN
+    DELETE cfc FROM checklist_for_company cfc JOIN checklist c ON (cfc.checklist_id = c.id)
+    WHERE cfc.status = NULL AND cfc.date = NOW();
 END //
 
 -- ------------------------------
@@ -142,10 +152,13 @@ END //
 SET GLOBAL event_scheduler = ON//
 
 -- -----------------------------------------------------
--- Создаем событие для ежедневного выполнения процедуры
+-- Создаем событие для ежедневного выполнения процедур
 -- -----------------------------------------------------
-CREATE EVENT IF NOT EXISTS auto_checklist_cleanup ON SCHEDULE EVERY 1 DAY STARTS CURRENT_TIMESTAMP DO
-CALL cleanup_expired_checklist_for_company()//
+CREATE EVENT IF NOT EXISTS auto_checklist_cleanup ON SCHEDULE EVERY 1 HOUR STARTS CURRENT_TIMESTAMP DO
+BEGIN
+	CALL cleanup_expired_checklist_for_company1();
+	CALL cleanup_expired_checklist_for_company2();
+END//
 -- --------------------------------------------------------------------------------------------------------------------------
 
 /*  test
